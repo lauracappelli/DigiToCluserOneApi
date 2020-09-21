b677cfd2cc83763f1ce86002caeeda5d5a200444
lauracappelli <laura.cappelli6@studio.unibo.it>
2020-09-21 12:19:20 +0200

    inserimento dei file per la conversione digi to cluster

---
+++ digitocluster_oneapi.h
@@ -0,0 +1,28 @@
+#ifndef rawtodigi_oneapi_h_
+#define rawtodigi_oneapi_h_
+
+#include <CL/sycl.hpp>
+
+class Input;
+class Output;
+
+namespace oneapi{
+
+  void Digitocluster(const Input* input_d,
+                     Output* output_d,
+                     const uint32_t wordCounter,
+                     bool useQualityInfo,
+                     bool includeErrors,
+                     bool debug,
+#if __SYCL_COMPILER_VERSION <= 20200118
+                     // Intel oneAPI beta 4
+                     cl::sycl::ordered_queue queue);
+#else
+                     // Intel SYCL branch
+                     cl::sycl::queue queue);
+#endif
+} //namespace oneapi
+
+#endif  // rawtodigi_oneapi_h_
+
+
